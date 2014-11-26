e7aa10a1b5d1fa0464445c732c1f7a48914c29da
Augusto Roccasalva <augusto@rioplomo.com.ar>
2014-11-26 16:36:56 -0300

    Add attendance record model

--- hr/models.py
+++ hr/models.py
@@ -8,6 +8,16 @@ from flask.ext.sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
+class AttendanceRecord(db.Model):
+    __tablename__ = 'attendance_record'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_code = db.Colun(db.Integer, nullable=False)
+    datetime = db.Column(db.DateTime, nullable=False, unique=True)
+    bkp_type = db.Column(db.Integer, nullable=False)
+    type_code = db.Column(db.Integer, nullable=False)
+
+
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
