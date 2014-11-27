98fb4828c90d98307bc7dca113034f50a9b63c5c
Augusto Roccasalva <augusto@rioplomo.com.ar>
2014-11-27 11:13:09 -0300

    Some progress on front end

--- hr/views.py
+++ hr/views.py
@@ -7,6 +7,22 @@ from hr.models import db, Employee
 
 @app.route("/")
 def index():
+    return render_template("index.html")
+
+@app.route("/employee")
+def employee_list():
     employees = Employee.query.order_by(Employee.file_no)\
                               .order_by(Employee.hire_date)
     return render_template("employee_list.html", employees=employees)
+
+@app.route("/employee/add")
+def employee_add():
+    pass
+
+@app.route("/employee/<id>")
+def employee_view(id):
+    pass
+
+@app.route("/employee/<id>/edit")
+def employee_edit(id):
+    pass
