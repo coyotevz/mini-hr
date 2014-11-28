bb6e93bf699ee23db0ca91e5e0a21fa9ae97ce35
Augusto Roccasalva <augusto@rioplomo.com.ar>
2014-11-28 04:59:49 -0300

    Start working times table generation

---
+++ gen_table.py
@@ -0,0 +1,7 @@
+# This requires `pip install business-calendar`
+
+
+from datetime import date
+from busines_calendar import Calendar
+
+_url = "https://mozorg.cdn.mozilla.net/media/caldata/ArgentinaHolidays.ics"
