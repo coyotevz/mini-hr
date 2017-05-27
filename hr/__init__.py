c1829d4e1ecc4b800f534cd1eddc9091066132be
Augusto Roccasalva <augusto@rioplomo.com.ar>
2017-05-27 04:17:19 -0300

    Fix prev/next period calculation

--- hr/__init__.py
+++ hr/__init__.py
@@ -23,4 +23,24 @@ def static_processor():
         return url_for('static', filename=filename)
     return dict(static=static)
 
+
+@app.context_processor
+def period_processor():
+    def next_period(year, month):
+        month = month + 1
+        if month > 12:
+            month = month - 12
+            year = year + 1
+        return year, month
+
+    def prev_period(year, month):
+        month = month - 1
+        if month < 1:
+            month = month + 12
+            year = year - 1
+        return year, month
+
+    return dict(next_period=next_period,
+                prev_period=prev_period)
+
 import hr.views
